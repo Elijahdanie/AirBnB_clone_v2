af039d3b6db79e42bd1ffa023b8fcc26f08399a8
Elijahdanie <elijahdanie@gmail.com>
2021-12-20 13:29:56 +0100

    modified create function

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -112,17 +112,44 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overrides the emptyline method of CMD """
         pass
+    
+    def ExtractArgs(args):
+        """ This method extracts
+        keyword arguments like name="Holberton"
+        in groups of <params> <params> <params>
+        in a string, where params is the keyward arguments
+         """
+        parse_kwargs = args[1:]
+        final_parseArgs = {}
+        for i in parse_kwargs:
+            k, v = i.split("=")
+            typecheck = type(eval(v))
+            if typecheck in [int, float, str]:
+                if typecheck is int:
+                    final_parseArgs[k] = int(v)
+                if typecheck is float:
+                    final_parseArgs[k] = float(v)
+                if typecheck is str:
+                    v = str(v[1:-1])
+                    v = v.replace('_', ' ')
+                    final_parseArgs[k] = v
+        return final_parseArgs
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        check_arg = args.split()
+        if not check_arg[0]:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif check_arg[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        new_instance = HBNBCommand.classes[check_arg[0]]()
+        if len(check_arg) > 1:
+            final_parseArgs = HBNBCommand.ExtractArgs(check_arg)
+            for k, v in final_parseArgs.items():
+                setattr(new_instance, k, v)
+        new_instance.save()
         print(new_instance.id)
         storage.save()
 
@@ -272,7 +299,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +307,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
-            # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            # check for quoted val argreate
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
