a68e1f940a669ad469e2d94ed448899b77ded2b0
krummitz <amidbidee@gmail.com>
2021-12-22 02:32:03 +0100

    ready for tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime
+)
 import uuid
 from datetime import datetime
 
+from sqlalchemy.sql.expression import delete
 
+Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+
+
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = Column(String(60), primary_key=True, nullable=False)
+            self.created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+            self.updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +41,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -39,6 +50,11 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        from models import storage
+        storage.delete(self.id)
