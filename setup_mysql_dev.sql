66e3660f07d34e6f3df1734a9bca38cc96ae5c1f
krummitz <amidbidee@gmail.com>
2021-12-23 06:21:34 +0100

    fixes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,17 @@
--- prepares a MySQL server for the project 
-CREATE IF NOT EXISTS DATABASE hbnb_dev_db;
-USE hbnb_dev_db;
-CREATE USER `hbnb_dev`@`localhost` IDENTIFIED BY `hbnb_dev_pwd`;
-GRANT ALL PRIVILEDGES ON `hbnb_dev_db`.* TO `hbnb_dev` 
-IDENTIFIED BY `hbnb_dev_pwd`;
-GRANT SELECT PRIVILEGES ON `performance_schema`.* TO `hbnb_dev`
-IDENTIFIED BY `hbnb_dev_pwd`;
+-- Write a script that prepares a MySQL server --
+-- Create db 'hbnb_dev_db' --
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Add new user 'hbnb_dev' in localhost --
+-- User 'hbnb_dev' password should be 'hbnb_dev_pwd' --
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- User 'hbnb_dev' should have SELECT privileges on db 'performance_schema' --
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
+
+-- User 'hbnb_dev' should have all privileges on db 'hbnb_dev_db' --
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,17 @@
--- prepares a MySQL server for the project
-CREATE IF NOT EXISTS DATABASE hbnb_test_db;
-USE hbnb_test_db;
-CREATE USER `hbnb_test`@`localhost` IDENTIFIED BY `hbnb_dev_pwd`;
-GRANT ALL PRIVILEDGES ON `hbnb_test_db`.* TO `hbnb_test`
-IDENTIFIED BY `hbnb_test_pwd`;
-GRANT SELECT PRIVILEGES ON `performance_schema`.* TO `hbnb_test`
-IDENTIFIED BY `hbnb_test_pwd`;
+-- Write a script that prepares a MySQL server --
+-- Create db 'hbnb_test_db' --
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Add new user 'hbnb_test' in localhost --
+-- User 'hbnb_test' password should be 'hbnb_test_pwd' --
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+
+-- User 'hbnb_test' should have all privileges on db 'hbnb_test_db' --
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+
+-- User 'hbnb_test' should have SELECT privileges on db 'performance_schema --
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
