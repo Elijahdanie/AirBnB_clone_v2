5c56aa8adbe30977994cfee488e4dd89750a9c5e
Elijahdanie <elijahdanie@gmail.com>
2021-12-25 07:42:07 +0100

    made modifications to sql scripts

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,12 @@
--- Write a script that prepares a MySQL server -
--- Create db 'hbnb_dev_db' -
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Creates a database, creates a user and assigns Select privileges
+-- to the user hbnb_dev on performance schema
 
--- Add new user 'hbnb_dev' in localhost --
--- User 'hbnb_dev' password should be 'hbnb_dev_pwd' --
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
-
--- User 'hbnb_dev' should have SELECT privileges on db 'performance_schema' --
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
-
--- User 'hbnb_dev' should have all privileges on db 'hbnb_dev_db' --
-GRANT ALL PRIVILEGES ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost';
+CREATE IF NOT EXISTS DATABASE `hbnb_dev_db`;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost`
+IDENTIFIED BY `hbnb_dev_pwd`;
+GRANT ALL PRIVILEDGES ON `hbnb_dev_db`.* TO `hbnb_dev` 
+IDENTIFIED BY `hbnb_dev_pwd`;
+GRANT SELECT PRIVILEGES ON `performance_schema`.* TO `hbnb_dev`
+IDENTIFIED BY `hbnb_dev_pwd`;
 FLUSH PRIVILEGES;
