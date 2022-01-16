87f3ee1921946d6bebac785fe314c99a4968839e
Elijahdanie <elijahdanie@gmail.com>
2022-01-16 20:44:42 +0100

    ..

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -19,14 +19,12 @@ def do_clean(number=0):
     iteration = 1 if number == 0 else number
     remote_dir = '/data/web_static/releases/'
 
-    "clean locally"
     list_dir = os.listdir('versions')
     list_dir.sort()
     dir_len = len(list_dir)
     for dir_index in range(iteration):
         local('rm -r ./versions/{}'.format(list_dir[dir_index]))
     
-    "clean remotely"
     r_dir_list = run('ls -tr {}'.format(remote_dir))
     for ri in range(iteration):
         run('rm -r {}{}'.format(r_dir_list[ri]))
