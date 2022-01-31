698113706810079a205233e7d1632938f8be1b01
Elijahdanie <elijahdanie@gmail.com>
2022-01-31 19:42:11 +0100

    made few changes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -139,7 +139,7 @@ class DBStorage:
 
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(
-            bind=self.__engine, expire_on_commit=False))
+            bind=self.__engine, expire_on_commit=False))()
 
     def close(self):
         """
